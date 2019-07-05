6db6de0d33b2cdd777b726917e9a2f64361cc422
chankruze <chankruze@gmail.com>
2019-07-05 10:58:54 +0530

    on7xelte: Update Tree

--- omni_on7xelte.mk
+++ omni_on7xelte.mk
@@ -7,15 +7,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := on7xelte
 PRODUCT_NAME := omni_on7xelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-G610F
 
 # enable stock zip packages flash
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
